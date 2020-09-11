7fb223474947742d14ea4e2f27e95d79e11fdb7d
Belen Mareque <belen.mareque@gmail.com>
2020-09-11 18:38:30 -0300

    movimiento texto

--- js/movimiento-texto.js
+++ js/movimiento-texto.js
@@ -3,8 +3,10 @@ function scrollAppear() {
   var introText = document.querySelector('.text-intro');
    var introPosition = introText.getBoundingClientRect().top;
    var screenPosition = window.innerHeight ;
+
    if(introPosition < screenPosition){
      introText.classList.add('intro-appear');
    }
 }
   window.addEventListener('scroll', scrollAppear);
+
